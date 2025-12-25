f20388980351b21e9570a111f2729068eefd61d5
Satyam Gupta <stymrj@Satyams-MacBook-Air.local>
2025-12-25 11:57:56 +0530

    deleteFrom Cart & cartSummary

--- src/Controller/CartController.js
+++ src/Controller/CartController.js
@@ -46,10 +46,52 @@ const viewCart = async (req, res) => {
         const user = await User.findById(req.user._id); 
         res.status(200).json({ cart: user.cart });
     } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+const deleteFromCart = async(req,res)=>{
+    try {
+        const { id } = req.params
+        const user = await User.findById(req.user._id)
+
+        //checking if it is present in cart
+
+        const newCart = user.cart.filter((item)=>{
+            return item.product.toString() !== id.toString()
+        })
+
+        user.cart = newCart
+        await user.save()
+
+        res.status(200).json({msg : 'Product Removed From Cart', newCart})
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+
+const cartSummary = async(req,res)=>{
+    try {
+        const user = await User.findById(req.user._id).populate('cart.product')
+
+        let totalSum = 0
+        let totalProduct = 0
+
+        for(let item of user.cart){
+            totalProduct += item.quantity
+            totalSum += item.product.price * item.quantity
+        }
+
+        res.status(200).json({totalProduct, totalSum, cart : user.cart})
+        
+    } catch (error) {
         res.status(400).json({ error: error.message });
     }
 }
 
+
+
 module.exports = {
-    addToCart, viewCart
+    addToCart, viewCart, deleteFromCart, cartSummary
 }
--- src/routes/CartRouter.js
+++ src/routes/CartRouter.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { isBuyer } = require('../Middleware/isBuyer');
 const { isLoggedIn } = require('../Middleware/isLoggedIn');
-const { viewCart, addToCart } = require('../Controller/CartController');
+const { viewCart, addToCart, deleteFromCart, cartSummary } = require('../Controller/CartController');
 const router = express.Router();
 
 
 router.get('/cart',isLoggedIn, isBuyer, viewCart);
 router.post('/cart/:id',isLoggedIn, isBuyer, addToCart);
+router.delete('/cart/:id', isLoggedIn, isBuyer, deleteFromCart)
+router.get('/cart/summary', isLoggedIn, isBuyer, cartSummary)
 
 
 
